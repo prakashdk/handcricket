214ae32485a8c4d5bb831bb318248714bc627c2c
Prakash <54474725+prakashdk@users.noreply.github.com>
2022-01-17 11:05:24 +0530

    Update install_maven.sh

--- install_maven.sh
+++ install_maven.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 sudo yum install git -y
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
